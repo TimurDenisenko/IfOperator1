7ed169227d5a88c1babeea7729264d1b6a5ac60a
TimurDenisenko <timur.denisenko.2887@gmail.com>
2023-01-10 01:23:00 +0200

    IfOperator+While newV

--- IfOperator.py
+++ IfOperator.py
@@ -39,25 +39,25 @@ else:
 #3
 print("")
 print("3 Harjutus")
-kp1=input("Ristkülikukujulise põranda esimese külje pikkus - ")
+kp1=input("Ristkülikukujulise põranda esimese külje pikkus - ").replace(",",".",1)
 while kp1.replace(".","",1).isdigit()==False or float(kp1)==0:
     print("Palun kirjuta õige number")
-    kp1=input("Ristkülikukujulise põranda esimese külje pikkus - ")
-kp2=input("Ristkülikukujulise põranda teiseks külje pikkus - ")
+    kp1=input("Ristkülikukujulise põranda esimese külje pikkus - ").replace(",",".",1)
+kp2=input("Ristkülikukujulise põranda teiseks külje pikkus - ").replace(",",".",1)
 while kp2.replace(".","",1).isdigit()==False or float(kp2)==0:
     print("Palun kirjuta õige number")
-    kp2=input("Ristkülikukujulise põranda teiseks külje pikkus - ")
+    kp2=input("Ristkülikukujulise põranda teiseks külje pikkus - ").replace(",",".",1)
 kp1=float(kp1)
 kp2=float(kp2)
 S=kp1 * kp2
 print(f"Teie põrandapind - {S}")
 remont=input("Kas soovite põrandat renoveerida? (jah või ei) ").lower()
 while remont not in ["jah","ei"]:
-    remont=input("Palun kirjuta jah või ei - ").lower()
+    remont=input("Palun kirjuta jah või ei - ").lower().replace(",",".",1)
 if remont=="jah":
     hind=input("Kui palju maksab üks ruutmeeter eurodes? ")
     while hind.replace(".","",1).isdigit()==False or float(hind)==0:
-        hind=input("Kirjuta õige hindes ")
+        hind=input("Kirjuta õige hindes ").replace(",",".",1)
     hind=float(hind)
     hindS = hind * S
     print(f"Remondi hind - {hindS} euro")
@@ -67,9 +67,9 @@ else:
 #4
 print("")
 print("4 Harjutus")
-hind = input("Palju ese maksab? ")
+hind = input("Palju ese maksab? ").replace(",",".",1)
 while hind.replace(".","",1).isdigit()==False or float(hind)==0:
-    hind=input("Kirjuta õige hind! ")
+    hind=input("Kirjuta õige hind! ").replace(",",".",1)
 hind=float(hind)
 if hind>700 :
     hindP= (hind * 30)/100
@@ -80,9 +80,9 @@ else:
 #5
 print("")
 print("5 Harjutus")
-grad=input("Mis on temperatuur majas? ")
+grad=input("Mis on temperatuur majas? ").replace(",",".",1)
 while grad.replace(".","",1).replace("-","",1).isdigit()==False :
-    grad=input("Kirjuta õige temperatuur! ")
+    grad=input("Kirjuta õige temperatuur! ").replace(",",".",1)
 grad=float(grad)
 if grad>=18:
     print("Suurepärane temperatuur!")
@@ -92,9 +92,9 @@ else:
 #6
 print("")
 print("6 Harjutus")
-pikkus=input("Kui pikkus sa oled? ")
+pikkus=input("Kui pikkus sa oled? ").replace(",",".",1)
 while pikkus.replace(".","",1).isdigit()==False or int(pikkus)!=float(pikkus) or float(pikkus)<70 or float(pikkus)>250:
-    pikkus=input("Kirjuta õige pikkus! ")
+    pikkus=input("Kirjuta õige pikkus! ").replace(",",".",1)
 pikkus=float(pikkus)
 if pikkus<=150:
     print("Sa oled lühike")
@@ -109,9 +109,9 @@ print("7 Harjutus")
 sugu=input("Kas sa oled mees või naine? ").lower()
 while sugu not in ["mees","naine"]:
     sugu=input("Kirjuta õige sugu!")
-pikkus=input("Kui pikkus sa oled? ")
+pikkus=input("Kui pikkus sa oled? ").replace(",",".",1)
 while pikkus.replace(".","",1).isdigit()==False or int(pikkus)!=float(pikkus) or float(pikkus)<70 or float(pikkus)>250:
-    pikkus=input("Kirjuta õige pikkus! ")
+    pikkus=input("Kirjuta õige pikkus! ").replace(",",".",1)
 pikkus=float(pikkus)
 if sugu=="mees":
     if pikkus<165:
@@ -135,65 +135,50 @@ piima=input("Kas sa tahad osta piima? (jah või ei) ").lower()
 while piima not in ["jah", "ei"]:
     piima=input("Kirjuta ainult jah või ei! ").lower()
 if piima=="jah":
-    hindPiima=input("Kui palju maksab piima? ")
+    hindPiima=input("Kui palju maksab piima? ").replace(",",".",1)
     while hindPiima.replace(".","",1).isdigit==False or float(hindPiima)==0:
-        hindPiima=input("Kirjutage õige hind! ")
+        hindPiima=input("Kirjutage õige hind! ").replace(",",".",1)
     kogusPiima=input("Kui palju sa tahad osta? ")
     while kogusPiima.isdigit()==False or int(kogusPiima)==0:
         kogusPiima=input("Kirjuta õige arv! ")
+else:
+    hindPiima=kogusPiima=0
 sai=input("Kas sa tahad osta sai (jah või ei) ").lower()
 while sai not in ["jah", "ei"]:
     sai=input("Kirjuta ainult jah või ei! ").lower()
 if sai=="jah":
-    hindSai=input("Kui palju maksab Sai? ")
+    hindSai=input("Kui palju maksab Sai? ").replace(",",".",1)
     while hindSai.replace(".","",1).isdigit==False or float(hindSai)==0:
-        hindSai=input("Kirjutage õige hind! ")
+        hindSai=input("Kirjutage õige hind! ").replace(",",".",1)
     kogusSai=input("Kui palju sa tahad osta? ")
     while kogusSai.isdigit()==False or int(kogusSai)==0:
         kogusSai=input("Kirjuta õige arv! ")
+else:
+    hindSai=kogusSai=0
 leib=input("Kas sa tahad osta leib (jah või ei) ").lower()
 while leib not in ["jah", "ei"]:
     leib=input("Kirjuta ainult jah või ei! ").lower()
 if leib=="jah":
-    hindLeib=input("Kui palju maksab Leib? ")
+    hindLeib=input("Kui palju maksab Leib? ").replace(",",".",1)
     while hindLeib.replace(".","",1).isdigit==False or float(hindLeib)==0:
-        hindLeib=input("Kirjutage õige hind! ")
+        hindLeib=input("Kirjutage õige hind! ").replace(",",".",1)
     kogusLeib=input("Kui palju sa tahad osta? ")
     while kogusLeib.isdigit()==False or int(kogusLeib)==0:
         kogusLeib=input("Kirjuta õige arv! ")
-if piima=="ei" and sai=="ei" and leib=="ei":
-    print("Ostude summa on 0")
-elif piima=="ei" and sai=="ei" and leib=="jah":
-    OS=hindLeib * kogusLeib
-    print(f"Ostude summa on {OS}")
-elif piima=="ei" and sai=="jah" and leib=="ei":
-    OS=hindSai * kogusSai
-    print(f"Ostude summa on {OS}")
-elif piima=="ei" and sai=="jah" and leib=="jah":
-    OS=hindSai * kogusSai + hindLeib * kogusLeib
-    print(f"Ostude summa on {OS}")
-elif piima=="jah" and sai=="ei" and leib=="ei":
-    OS=hindPiima * kogusPiima
-    print(f"Ostude summa on {OS}")
-elif piima=="jah" and sai=="ei" and leib=="jah":
-    OS=hindPiima * kogusPiima + hindLeib * kogusLeib
-    print(f"Ostude summa on {OS}")
-elif piima=="jah" and sai=="jah" and leib=="ei":
-    OS=hindPiima * kogusPiima + hindSai * kogusSai
-    print(f"Ostude summa on {OS}")
 else:
-    OS=hindPiima*kogusPiima + hindSai*kogusSai + hindLeib*kogusLeib
-    print(f"Ostude summa on {OS}")
+    hindLeib=kogusLeib=0
+OS=float(hindPiima)*float(kogusPiima)+float(hindSai)*float(kogusSai)+float(hindLeib)*float(kogusLeib)
+print(f"Ostude summa on {OS}")
 
 #9
 print("")
 print("9 harjutus")
-a=input("Kirjutage 1 külje pikkus - ")
+a=input("Kirjutage 1 külje pikkus - ").replace(",",".",1)
 while a.replace(".","",1).isdigit()==False or float(a)==0:
-    a=input("Kirjuta õige pikkus ")
-b=input("Kirjutage 2 külje pikkus - ")
+    a=input("Kirjuta õige pikkus ").replace(",",".",1)
+b=input("Kirjutage 2 külje pikkus - ").replace(",",".",1)
 while b.replace(".","",1).isdigit()==False or float(b)==0:
-    b=input("Kirjuta õige pikkus ")
+    b=input("Kirjuta õige pikkus ").replace(",",".",1)
 if a==b:
     print("See on ruut!")
 else:
@@ -202,12 +187,12 @@ else:
 #10
 print("")
 print("10 harjutus")
-a=input("Kirjutage esimene number ")
+a=input("Kirjutage esimene number ").replace(",",".",1)
 while a.replace(".","",1).isdigit()==False:
-    a=input("Kirjutage õige number ")
-b=input("Kirjutage teine number ")
+    a=input("Kirjutage õige number ").replace(",",".",1)
+b=input("Kirjutage teine number ").replace(",",".",1)
 while b.replace(".","",1).isdigit()==False:
-    b=input("Kirjuta õige number ")
+    b=input("Kirjuta õige number ").replace(",",".",1)
 a=float(a)
 b=float(b)
 s=input("Kirjutage tegevuse märk (+ - * /) ")
@@ -243,9 +228,9 @@ else:
 #12
 print("")
 print("12 harjutus")
-hind=input("Kirjutage kauba hind ")
+hind=input("Kirjutage kauba hind ").replace(",",".",1)
 while hind.replace(".","",1).isdigit()==False or float(hind)==0:
-    hind=input("Kirjutage õige kauba hind ")
+    hind=input("Kirjutage õige kauba hind ").replace(",",".",1)
 if hind<=10:
     summ=hind - (hind*10)/100
     print(f"Toote hind on {summ}")
